95e9d36be8c52204bd00bd84a79fa56d0080a7c8
ashkansamadiyan <158470661+ashkansamadiyan@users.noreply.github.com>
2025-02-09 17:43:46 +0330

    Update main.py

--- py/main.py
+++ py/main.py
@@ -3,14 +3,11 @@ import time
 
 import uvloop
 
-# Use uvloop as the default event loop for enhanced performance.
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 
 async def call_db(delay: float, i: int, results: list):
-    # Simulate a delay (e.g. a database call)
     await asyncio.sleep(delay)
-    # Append the index to the results list.
     results.append(i)
 
 
@@ -19,16 +16,13 @@ async def main():
     start_time = time.time()
 
     results = []  # Shared results list
-    # Prepare a list of tasks. Each task will wait for `delay` seconds
-    # and then add its index to the results.
+
     tasks = [asyncio.create_task(call_db(3, i, results)) for i in range(5_000_000)]
 
-    # Await all tasks to finish.
     await asyncio.gather(*tasks)
 
     elapsed = time.time() - start_time
     print(f"{elapsed} seconds")
-    # Optionally inspect the results:
     # print(results)
 
 
